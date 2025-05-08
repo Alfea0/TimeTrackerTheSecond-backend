619caa07d0add264d7ddb2a6961ec95f18112d15
Alice Fearn <alice.fearn@hotmail.com>
2025-05-08 14:28:13 +0200

    TaskController

---
+++ src/main/java/com/TimeTrackerTheSecond_backend/controller/TaskController.java
@@ -0,0 +1,38 @@
+package com.TimeTrackerTheSecond_backend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.TimeTrackerTheSecond_backend.model.Task;
+import com.TimeTrackerTheSecond_backend.service.TaskService;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable String id, @RequestBody Task task) {
+        return taskService.updateTask(id, task);
+    }
+}
