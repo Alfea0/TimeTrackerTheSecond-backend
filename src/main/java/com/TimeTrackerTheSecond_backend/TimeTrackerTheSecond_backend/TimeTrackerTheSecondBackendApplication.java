f917e586e7c6ee8fbf1dcd9f51bdb02172988215
Alice Fearn <alice.fearn@hotmail.com>
2025-05-08 13:22:46 +0200

    spring projekt

---
+++ src/main/java/com/TimeTrackerTheSecond_backend/TimeTrackerTheSecond_backend/TimeTrackerTheSecondBackendApplication.java
@@ -0,0 +1,13 @@
+package com.TimeTrackerTheSecond_backend.TimeTrackerTheSecond_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTrackerTheSecondBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTrackerTheSecondBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TimeTrackerTheSecond_backend/TimeTrackerTheSecond_backend/TimeTrackerTheSecondBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TimeTrackerTheSecond_backend.TimeTrackerTheSecond_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTrackerTheSecondBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
