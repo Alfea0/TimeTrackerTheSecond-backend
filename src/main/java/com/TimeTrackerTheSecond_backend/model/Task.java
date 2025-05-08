6d4b537cb22a578c91a1d80c0e0e2ce4d725e2d5
Alice Fearn <alice.fearn@hotmail.com>
2025-05-08 13:44:02 +0200

    model för arbetsuppgifter

---
+++ src/main/java/com/TimeTrackerTheSecond_backend/model/Task.java
@@ -0,0 +1,38 @@
+package com.TimeTrackerTheSecond_backend.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+// model som ska användas för att spara arbetsuppgifter i databasen
+@Document(collection = "tasks")
+public class Task {
+
+    @Id
+    private String id;
+    private String name;
+    private boolean isActive;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
